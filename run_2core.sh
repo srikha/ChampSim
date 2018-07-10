c4c7626c264e630daa99a981a433e019580b587b
Sri <srikha13@gmail.com>
2018-07-10 18:45:33 +0530

    commit

--- run_2core.sh
+++ run_2core.sh
@@ -8,9 +8,9 @@ trace2=${5}
 option=''
 
 #trace1=`sed -n ''$num'p' sim_list/4core_workloads.txt | awk '{print $1}'`
-#trace2=`sed -n ''$num'p' sim_list/4core_workloads.txt | awk '{print $2}'`
+#trace2=`sed -n ''$num'p' sim_list/4core_workloads.txt | awk '{print $2}'`  
 
 mkdir -p results_2core
 
-(./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz &> results_2core/mix${num}-${binary}${option}.txt)
+(./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}0000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz &> results_2core/mix${num}-${binary}${option}-${trace2}.txt)
 #gdb --args ./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz
